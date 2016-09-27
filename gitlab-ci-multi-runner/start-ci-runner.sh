04841d6904823ff293035aa456853d89309f5a99
msdx <645079761@qq.com>
2016-09-27 09:56:53 +0800

    Add gitlab-ci-multi-runner Dockerfile

---
+++ gitlab-ci-multi-runner/start-ci-runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo docker run -d \
+  --name gitlab-ci-multi-runner \
+  --restart always \
+  -v /home/msdx/androd-sdk:/android-sdk:ro \
+  myrepo/gitlab-ci-runner:registered \
+  gitlab-ci-multi-runner --debug run
